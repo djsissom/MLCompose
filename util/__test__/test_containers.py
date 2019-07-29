d811f76b3de3d6d8174283f1e1abe0144b88ba83
Daniel Sissom <daniel@arclet.org>
2019-07-29 12:15:45 -0500

    Added unit tests for util metaclass containers.

---
+++ util/__test__/test_containers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import unittest
+
+from .. import containers
+
+
+
+class InheritanceTestClass1(containers.CheckArg):
+	pass
+
+
+
+class InheritanceTestClass2(containers.CheckArg):
+	def __init__(self, arg):
+		print(arg)
+
+
+
+class TestMetaClasses(unittest.TestCase):
+	def test_passing_existing_instance_of_same_class_as_arg(self):
+		a = containers.CheckArg()
+		b = containers.CheckArg(a)
+		self.assertTrue(a is b)
+		return
+
+
+	def test_passing_existing_instance_of_different_class_as_arg(self):
+		a = InheritanceTestClass1()
+		b = InheritanceTestClass2(a)
+		self.assertTrue(a is not b)
+		return
+
+
+
+if __name__ == "__main__":
+	unittest.main()
+
