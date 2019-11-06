a82d334389a3b5a1e718f29518f324a6cc9845c7
Daniel Sissom <daniel@arclet.org>
2019-11-06 16:25:45 -0600

    Added file placeholders for learn module.

---
+++ learn/__init__.py
@@ -0,0 +1,11 @@
+__all__ = ['models', 'optimize', 'preprocess']
+
+from . import models
+from . import optimize
+from . import preprocess
+
+#preprocess_data = preprocess.preprocess_data
+#preprocess_targets = preprocess.preprocess_targets
+#import_model = models.import_model
+#get_best_model = optimize.get_best_model
+
