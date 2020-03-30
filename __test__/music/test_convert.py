7ff8f304febe5766c21cb72578527bed65b374a6
Daniel Sissom <daniel@arclet.org>
2020-03-30 17:21:49 -0500

    Updated midi convert unit test to create midi file.

--- __test__/music/test_convert.py
+++ __test__/music/test_convert.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
 import unittest
-
-from mlcompose.music import containers
-from mlcompose.music import convert
+from mlcompose import music as mus
 
 
 
 class TestMidi(unittest.TestCase):
 	def test_saving_song_as_midi_file(self):
-		a = containers.Song()
+		song = mus.Song()
+		track = song.add_track()
+		measure = track.append_measure()
+		beat = measure.append_beat()
+		note = beat.add_note(mus.Note('C_6'))
+		song.end_song()
+
+		song.to_midi('test.mid')
 		#self.assertTrue(a is b)
+		# TODO:  check created file against known good file
 		return
 
 
